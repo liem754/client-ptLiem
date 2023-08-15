0d2be9f4cf07568778993614ea5b53020ffc5a5c
Your <you@example.com>
2023-08-15 15:58:07 +0700

    fix filter

--- src/containers/public/Homepage.js
+++ src/containers/public/Homepage.js
@@ -24,67 +24,74 @@ function Homepage() {
         <span className="text-sm text-black-200">{test.HOME_BODY}</span>
       </div>
       <Provinces />
-      <div className="bg-white flex gap-3 md:hidden mt-10">
-        <div className="mt-5 flex flex-col gap-2 pl-3 flex-1">
-          <h1 className="border-2 border-black py-1 px-1 text-[12px] text-center font-medium">
+      <div className="bg-white flex flex-col gap-3 md:hidden mt-10">
+        <div className="mt-5 flex flex-col gap-2 ">
+          <h1 className="border-2 border-black  text-[12px] text-center font-medium">
             Chọn phương thức lọc
           </h1>
-          <div
-            className={`text-[12px] ${
-              show1 && "underline underline-offset-4 text-red-600"
-            }`}
-            onClick={() => {
-              setShow1(true);
-              setShow2(false);
-              setShow3(false);
-            }}
-          >
-            Danh sách cho thuê
-          </div>
-          <div
-            className={`text-[12px] ${
-              show2 && "underline underline-offset-4 text-red-600"
-            }`}
-            onClick={() => {
-              setShow1(false);
-              setShow2(true);
-              setShow3(false);
-            }}
-          >
-            Theo diện tích
-          </div>
-          <div
-            onClick={() => {
-              setShow1(false);
-              setShow2(false);
-              setShow3(true);
-            }}
-            className={`text-[12px] ${
-              show3 && "underline underline-offset-4 text-red-600"
-            }`}
-          >
-            Theo giá
+          <div className="flex pr-4 pl-2 justify-between">
+            <div
+              className={`text-[12px] font-medium hover:text-red-600 cursor-pointer ${
+                show1 && "underline underline-offset-4 text-red-600"
+              }`}
+              onClick={() => {
+                setShow1(true);
+                setShow2(false);
+                setShow3(false);
+              }}
+            >
+              Danh sách cho thuê
+            </div>
+            <div
+              className={`text-[12px] font-medium hover:text-red-600 cursor-pointer ${
+                show2 && "underline underline-offset-4 text-red-600"
+              }`}
+              onClick={() => {
+                setShow1(false);
+                setShow2(true);
+                setShow3(false);
+              }}
+            >
+              Theo diện tích
+            </div>
+            <div
+              onClick={() => {
+                setShow1(false);
+                setShow2(false);
+                setShow3(true);
+              }}
+              className={`text-[12px] font-medium hover:text-red-600 cursor-pointer ${
+                show3 && "underline underline-offset-4 text-red-600"
+              }`}
+            >
+              Theo giá
+            </div>
           </div>
         </div>
-        <div className="flex-1 mt-10">
+        <div className={"ml-1"}>
           {show1 && (
-            <ItemSidebar no text={categories} title="Danh sách cho thuê" />
+            <ItemSidebar
+              isdouble
+              no
+              text={categories}
+              title="Danh sách cho thuê"
+            />
           )}
           {show2 && (
             <ItemSidebar
+              isdouble
               no
               type="areaCode"
               text={areas}
-              isdouble
               title="Xem theo diện tích"
             />
           )}
           {show3 && (
             <ItemSidebar
+              isdouble
               no
               text={prices}
               type="priceCode"
-              isdouble
               title="Xem theo giá"
             />
           )}
